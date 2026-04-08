2ffcdeaf2258904edbf6575fe50400352ee7eeb3
TrungKien <mtk28705@gmail.com>
2026-04-08 18:57:14 +0700

    up

--- src/ConnectDB/ConnectDB.java
+++ src/ConnectDB/ConnectDB.java
@@ -1,11 +1,44 @@
 package ConnectDB;
 
-public class ConnectDB {
-	String id;
-	int abc;
-	int kien;
-	int diem;
-	int abcd;
-	String datdeptrai;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class ConnectDB {
+    private static Connection con = null;
+    private static ConnectDB instance = new ConnectDB();
+
+    public static ConnectDB getInstance() {
+        return instance;
+    }
+
+    public void connect() {
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=MYCAREPHARMACY;encrypt=false";
+        String user = "sa";
+        String password = "123456789";
+
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); // thêm dòng này
+
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Kết nối DB thành công!");
+        } catch (Exception e) {
+            System.out.println("Kết nối DB thất bại!");
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        if (con != null) {
+            try {
+                con.close();
+                System.out.println("Đã ngắt kết nối!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Connection getConnection() {
+        return con;
+    }
 }
