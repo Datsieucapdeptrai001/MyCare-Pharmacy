c7cdd685867d5f80f5ff71d821a94cdbab1c8e06
Thuantran2580 <tranlongthuan1866@gmail.com>
2026-04-09 20:32:00 +0700

    Thêm Dao_HinhThucKhuyenMai  ApDungKm Giảm theo % và tối đa đc bao nhiêu , Sửa Dao_ChitietHoaDon ,

--- src/DAO/DAO_HoaDon.java
+++ src/DAO/DAO_HoaDon.java
@@ -13,15 +13,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 public class DAO_HoaDon {
 
     public DAO_HoaDon() {}
 
     public boolean themHoaDon(HoaDon hd) {
-        // hoaDonGocId dùng cho trường hợp hóa đơn này là hóa đơn Đổi/Trả tham chiếu đến hóa đơn mua ban đầu
-        String sql = "INSERT INTO HoaDon (id, loaiHD, ghiChu, ngayLapHD, nhanVienId, KhachHangId, khuyenMaiId, phuongThucThanhToan, hoaDonGocId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO HoaDon (id, loaiHD, ghiChu, ngayLapHD, nhanVienId, khachHangId, khuyenMaiId, phuongThucThanhToan, hoaDonGocId) "
+                   + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
         int n = 0;
         Connection con = ConnectDB.getInstance().getConnection();
 
@@ -29,53 +29,102 @@ public class DAO_HoaDon {
             pst.setString(1, hd.getId());
             pst.setString(2, hd.getLoaiHD().name());
             pst.setString(3, hd.getGhiChu());
-            
-            LocalDateTime ngayLap = hd.getNgayLapHD() != null ? hd.getNgayLapHD() : LocalDateTime.now();
-            pst.setTimestamp(4, Timestamp.valueOf(ngayLap));
-            
-            pst.setString(5, hd.getNhanVienId() != null ? hd.getNhanVienId().getNhanVien() : null);
-            pst.setString(6, hd.getKhachHangId() != null ? hd.getKhachHangId().getId() : null);
-            pst.setString(7, hd.getKhuyenMaiId() != null ? hd.getKhuyenMaiId().getId() : null);
-            pst.setString(8, hd.getPhuongThucThanhToan() != null ? hd.getPhuongThucThanhToan().name() : null);
-            pst.setString(9, hd.getHoaDonGocId() != null ? hd.getHoaDonGocId().getId() : null);
+            pst.setTimestamp(4, Timestamp.valueOf(hd.getNgayLapHD()));
+
+            pst.setString(5, hd.getNhanVienId().getNhanVien());
+
+            if (hd.getKhachHangId() != null) {
+                pst.setString(6, hd.getKhachHangId().getId());
+            } else {
+                pst.setNull(6, java.sql.Types.NVARCHAR);
+            }
+
+            if (hd.getKhuyenMaiId() != null) {
+                pst.setString(7, hd.getKhuyenMaiId().getId());
+            } else {
+                pst.setNull(7, java.sql.Types.NVARCHAR);
+            }
+
+            pst.setString(8, hd.getPhuongThucThanhToan().name());
+
+            if (hd.getHoaDonGocId() != null) {
+                pst.setString(9, hd.getHoaDonGocId().getId());
+            } else {
+                pst.setNull(9, java.sql.Types.NVARCHAR);
+            }
 
             n = pst.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return n > 0;
     }
 
-    public HoaDon layHoaDonTheoMa(String maHoaDon) {
+    public HoaDon layHoaDonTheoMa(String maHD) {
         HoaDon hd = null;
         String sql = "SELECT * FROM HoaDon WHERE id = ?";
         Connection con = ConnectDB.getInstance().getConnection();
 
         try (PreparedStatement pst = con.prepareStatement(sql)) {
-            pst.setString(1, maHoaDon);
+            pst.setString(1, maHD);
+
             try (ResultSet rs = pst.executeQuery()) {
                 if (rs.next()) {
                     hd = new HoaDon();
                     hd.setId(rs.getString("id"));
-                    if (rs.getString("loaiHD") != null) hd.setLoaiHD(LoaiHoaDon.valueOf(rs.getString("loaiHD")));
                     hd.setGhiChu(rs.getString("ghiChu"));
-                    if (rs.getTimestamp("ngayLapHD") != null) hd.setNgayLapHD(rs.getTimestamp("ngayLapHD").toLocalDateTime());
-                    if (rs.getString("phuongThucThanhToan") != null) hd.setPhuongThucThanhToan(PhuongThucThanhToan.valueOf(rs.getString("phuongThucThanhToan")));
-                    
-                    // Bạn có thể set tạm ID cho các object liên kết (Lazy loading)
-                    // ... (set NhanVien, KhachHang)
+
+                    if (rs.getString("loaiHD") != null) {
+                        hd.setLoaiHD(LoaiHoaDon.valueOf(rs.getString("loaiHD")));
+                    }
+
+                    if (rs.getTimestamp("ngayLapHD") != null) {
+                        hd.setNgayLapHD(rs.getTimestamp("ngayLapHD").toLocalDateTime());
+                    }
+
+                    if (rs.getString("nhanVienId") != null) {
+                        NhanVien nv = new NhanVien();
+                        nv.setNhanVien(rs.getString("nhanVienId"));
+                        hd.setNhanVienId(nv);
+                    }
+
+                    if (rs.getString("khachHangId") != null) {
+                        KhachHang kh = new KhachHang();
+                        kh.setId(rs.getString("khachHangId"));
+                        hd.setKhachHangId(kh);
+                    }
+
+                    if (rs.getString("khuyenMaiId") != null) {
+                        KhuyenMai km = new KhuyenMai();
+                        km.setId(rs.getString("khuyenMaiId"));
+                        hd.setKhuyenMaiId(km);
+                    }
+
+                    if (rs.getString("phuongThucThanhToan") != null) {
+                        hd.setPhuongThucThanhToan(
+                            PhuongThucThanhToan.valueOf(rs.getString("phuongThucThanhToan"))
+                        );
+                    }
+
+                    if (rs.getString("hoaDonGocId") != null) {
+                        HoaDon hdGoc = new HoaDon();
+                        hdGoc.setId(rs.getString("hoaDonGocId"));
+                        hd.setHoaDonGocId(hdGoc);
+                    }
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return hd;
     }
 
-    // Hàm cập nhật trạng thái (Trong Entity của bạn đang quản lý bằng Enum LoaiHoaDon: BAN_HANG, TRA_HANG)
     public boolean capNhatTrangThai(String idHD, LoaiHoaDon loaiMoi) {
         String sql = "UPDATE HoaDon SET loaiHD = ? WHERE id = ?";
         Connection con = ConnectDB.getInstance().getConnection();
+
         try (PreparedStatement pst = con.prepareStatement(sql)) {
             pst.setString(1, loaiMoi.name());
             pst.setString(2, idHD);
@@ -83,6 +132,7 @@ public class DAO_HoaDon {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return false;
     }
 }
